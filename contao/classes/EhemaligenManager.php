40a6dba6ef9bca9362ea29f6e1996acfcd931daa
kozi <martin@kozianka.de>
2014-09-16 12:05:12 +0200

    CSV import and export based on https://github.com/thephpleague/csv/

--- contao/classes/EhemaligenManager.php
+++ contao/classes/EhemaligenManager.php
@@ -7,11 +7,15 @@
  * @author     Martin Kozianka <http://kozianka.de>
  * @package    ehemalige
  */
+use League\Csv\Writer;
+use League\Csv\Reader;
 
 class EhemaligenManager extends \System {
     private $filenamePrefix = '/files/ehemalige/ehemalige';
     private $filenameSuffix = '.csv';
     public function importCsv() {
+        $this->import('Database');
+
         $filename = $this->filenamePrefix.$this->filenameSuffix;
 
         if (!file_exists(TL_ROOT.$filename)) {
@@ -19,20 +23,37 @@ class EhemaligenManager extends \System {
             \Controller::redirect(\Environment::get('script').'?do=ehemalige');
         }
 
-        $strInput = file_get_contents(TL_ROOT.$filename);
-        if ($strInput === false) {
-            \Message::add(sprintf('Der Inhalt der Datei <strong>%s</strong> konnte nicht gelesen werden.', $filename), 'TL_ERROR');
-            \Controller::redirect(\Environment::get('script').'?do=ehemalige');
+
+        $reader = Reader::createFromPath(TL_ROOT.$filename);
+
+        // Check for correct headers
+        $headers = $reader->fetchOne();
+        foreach(\EhemaligeModel::$ARR_CSV_HEADER as $key) {
+            if (!in_array($key, $headers)) {
+                \Message::add(sprintf('Der Schlüssel <strong>%s</strong> fehlt in der Kopfzeile <strong>%s</strong> in der Datei %s.',
+                    $key, implode(', ', $headers), $filename), 'TL_ERROR');
+                \Controller::redirect(\Environment::get('script').'?do=ehemalige');
+            }
         }
 
-        $arrCsv = str_getcsv($strInput);
-        var_dump($arrCsv);
+        $this->Database->execute('TRUNCATE TABLE tl_ehemalige');
 
-            // str_getcsv — Parst einen CSV-String in ein Array
-            //  array str_getcsv ( string $input [, string $delimiter = ',' [, string $enclosure = '"' [, string $escape = '\\' ]]] )
-        \Message::add('Importiert', 'TL_INFO');
+        $arrRows = $reader->setOffset(1)->fetchAssoc($headers);
+        $count = 0;
+        foreach($arrRows as $row) {
+
+            if ($row['name'] !== null && strlen($row['name']) > 0 ) {
+                $row['tstamp'] = time();
+                $objEhemalige = new \EhemaligeModel();
+                $objEhemalige->setRow($row);
+                $objEhemalige->save();
+                $count++;
+            }
+        }
+
+        \Message::add(sprintf('Es wurden <strong>%s</strong> Einträge aus der Datei <strong>%s</strong> importiert.',
+            $count, $filename), 'TL_INFO');
         \Controller::redirect(\Environment::get('script').'?do=ehemalige');
-
 
     }
 
@@ -44,19 +65,17 @@ class EhemaligenManager extends \System {
             $filename = $this->filenamePrefix.'-'.\Date::parse('Y-m-d_h-i-s').$this->filenameSuffix;
         }
 
-        $fileHandle = fopen(TL_ROOT.$filename, 'w+');
+        $writer     = Writer::createFromPath(TL_ROOT.$filename, 'w+');
         $collection = \EhemaligeModel::findAll(array('order' => 'name ASC'));
 
         // CSV-Header schreiben
-        fputcsv($fileHandle, EhemaligeModel::$ARR_CSV_HEADER);
+        $writer->insertOne(EhemaligeModel::$ARR_CSV_HEADER);
         $count = 0;
         foreach($collection as $objEhemalige) {
             // CSV-Zeilen schreiben
-            fputcsv($fileHandle, $objEhemalige->getCsvArray());
+            $writer->insertOne($objEhemalige->getCsvArray());
             $count++;
         }
-        fclose($fileHandle);
-
         \Message::add(sprintf($message, $count, $filename), 'TL_INFO');
         \Controller::redirect(\Environment::get('script').'?do=ehemalige');
     }
